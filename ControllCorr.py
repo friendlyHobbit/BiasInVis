0a27681efc1a714c7053529b01f86593d98518b1
TeachSita <sitvri001@edu.rvn.se>
2022-11-28 14:16:02 +0100

    attempt to calculate outliers
    used IQR

--- ControllCorr.py
+++ ControllCorr.py
@@ -4,13 +4,14 @@ import numpy as np
 from scipy import stats 
 import pandas as pd 
 
+
 # set controlled variables
-n_data_points = 5
+n_data_points = 100
 stand_dev = 4
 set_corr = 0.4
 set_x = np.random.uniform(1, 20, n_data_points) 
 
-error = 0.02
+error = 0.01
 
 def CreateScatter(sd, x):
     temp_sd = sd
@@ -36,8 +37,6 @@ def CreateScatter(sd, x):
 
     return y
     
-print("type X ", type(set_x))
-print("type Y ", type(CreateScatter(stand_dev, set_x)))
 
 # get regression variables
 slope, intercept, r, p, std_err = stats.linregress(set_x, CreateScatter(stand_dev, set_x))
@@ -52,10 +51,41 @@ mymodel = list(map(myfunc, set_x))
 #predict_n = myfunc(10)
 #print("predict_n: ", predict_n)
 
+
+# calculate outliers based on IQR, give 1 dimentional data as input
+def OutlierDetector(d):
+    # calculate Q1 and Q3
+    q1, q3 = np.percentile(d, [25, 75])
+    iqr = q3 - q1
+    upperq = q3 + 1.5*iqr
+    lowerq = q1 - 1.5*iqr
+
+    return upperq, lowerq
+
+print("outlier x: ", OutlierDetector(set_x))
+#print("outlier y: ", OutlierDetector(CreateScatter(stand_dev, set_x)))
+
+
+# set color dependent on IQR, give upper, lower bounds and 1d data
+def SetOutlierColor(u, l, d):
+    for n in d:
+        # if outside IQR
+        if n > u:
+            clr = '#1f77b4'
+        elif n < l:
+            clr = '#1f77b4'
+        # else it´s outside IQR
+        else:
+            clr = '#bcbd22'
+    return clr
+
+
+
 # for each x value, calculate distance to regression line
 for y in CreateScatter(stand_dev, set_x):
     err = np.abs(y - myfunc(y))
-    print(err) 
+    #print(err) 
+
 
 # correlation coefficient
 print("r: ", r)
@@ -63,15 +93,17 @@ print("pearsons corr: ", stats.pearsonr(set_x, CreateScatter(stand_dev, set_x)))
 print("slope: ", slope)
 
 
-plt.scatter(set_x, CreateScatter(stand_dev, set_x), c='#000000', alpha=0.8)
+# get x points outside IQR
+upperqx, lowerqx = OutlierDetector(set_x)
+
+plt.scatter(set_x, CreateScatter(stand_dev, set_x), c=SetOutlierColor(upperqx, lowerqx, set_x), alpha=0.7)
 plt.plot(set_x, mymodel)
 
 # Plot formatting
-#plt.yticks(color='w')
-#plt.xticks(color='w')
+plt.yticks(color='w')
+plt.xticks(color='w')
 plt.xlabel("Drownings")
 plt.ylabel("Ice-cream sales")
-#plt.axis('equal')
 
 
 plt.show()
